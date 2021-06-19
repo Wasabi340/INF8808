b5cccfd4486a97f11fa61937c534171f664382bb
Alex <alex.trepanier@hotmail.com>
2021-06-19 18:36:24 -0400

    toggle switches

--- public/scripts/heatMaps.js
+++ public/scripts/heatMaps.js
@@ -7,6 +7,7 @@ function getFakeData(){
     for(let i = 0; i < 20; i++){
         
         data.studyCases.push({
+            name:`Case ${i+1}`,
             metric:Math.floor(Math.random() * 101),
             values:[]
         })
@@ -25,45 +26,122 @@ export function build () {
     let g = d3.select('.heat-maps svg')
     
     let fakeData = getFakeData()
-
-    let width = g.node().getBoundingClientRect().width
-    let height = g.node().getBoundingClientRect().height
-
-    let verticalScale = d3.scaleLinear()
-        .domain([0,fakeData.studyCases.length])
-        .range([0,height])
-
-    let horizontalScale = d3.scaleLinear()
-        .domain([0,fakeData.studyCases[0].values.length])
-        .range([0,width*0.75])
-
-    let colorScale = d3.scaleLinear()
-        .domain([0, 0.33, 0.66, 1])
-        .range(['blue', 'green', 'yellow', 'red'])
     
-    g.selectAll('g')
+    let maxWidth = g.node().getBoundingClientRect().width
+    let maxHeight = g.node().getBoundingClientRect().height
+    let margin = {
+        top:0.05,
+        bottom:0,
+        left:0.20,
+        right:0.20
+    }
+    
+    let verticalScale = d3.scaleLinear()
+    .domain([0, fakeData.studyCases.length])
+    .range([margin.top*maxHeight, maxHeight-margin.bottom*maxHeight])
+    
+    let horizontalScale = d3.scaleLinear()
+    .domain([0,fakeData.studyCases[0].values.length])
+    .range([margin.left*maxWidth,maxWidth-margin.right*maxWidth])
+    
+    let colorScale = d3.scaleLinear()
+    .domain([0, 0.33, 0.66, 1])
+    .range(['blue', 'green', 'yellow', 'red'])
+    
+    let init = g.selectAll('g')
     .data(fakeData.studyCases)
     .enter()
     .append('g')
-    .attr('class', 'test')
-    .attr('transform', (d, i) => `translate(0 ${verticalScale(i)})`)
-    .selectAll('rect')
+    
+    init.append('text')
+    .attr('class', 'name')
+    .text((d)=>d.name)
+    
+    init.append('text')
+    .attr('class', 'metric')
+    .text((d)=>d.metric)
+    
+    init.append('rect')
+    .attr('class', 'switch')
+    
+    init.append('circle')
+    .attr('class', 'toggle')
+    
+    init.selectAll('rect.map')
     .data((d) => d.values)
     .enter()
     .append('rect')
-    .attr('width', 10)
-    .attr('height', 10)
-    .attr('x', (d, i) => horizontalScale(i))
-    .attr('fill', (d) => colorScale(d))
-
-    let groups = g.selectAll('g')
-
-    groups.attr('transform', (d, i) => `translate(0 ${verticalScale(i)})`)
-
-    let rects = groups.selectAll('rect')
+    .attr('class', 'map')
+    .on("mouseover", handleMouseOver)
+    .on("mouseout", handleMouseOut);
     
-    rects.attr('width', width*0.75/24)
-    .attr('height', width*0.75/24*1.5)
+    let groups = g.selectAll('g')
+    groups.attr('transform', (d, i) => `translate(0 ${verticalScale(i)})`)
+    
+    let names = groups.selectAll('text.name')
+    names.attr('x', maxWidth/2)
+    .attr('y', 0)
+    .attr('text-anchor', 'middle')
+    
+    let metrics = groups.selectAll('text.metric')
+    metrics.attr('x', margin.left*maxWidth/2)
+    .attr('y', 10)
+    .attr('text-anchor', 'middle')
+    .attr('dominant-baseline', 'middle')
+    
+    let switches = groups.selectAll('rect.switch')
+    switches.attr('width', margin.right*maxWidth * 0.75)
+    .attr('height', 20)
+    .attr('x', maxWidth - margin.right*maxWidth/2 - margin.right*maxWidth * 0.75/2)
+    .attr('y', 0)
+    .attr('fill', 'silver')
+    .attr('stroke', 'black')
+    .attr('stroke-width', 1)
+    .attr('rx', 10)
+    .attr('ry', 10)
+    
+    let toggles = groups.selectAll('circle.toggle')
+    
+    let switchWidth = margin.right*maxWidth * 0.75
+    
+    let left = maxWidth - margin.right*maxWidth/2 - switchWidth*0.30
+    let right = maxWidth - margin.right*maxWidth/2 + switchWidth*0.30
+    
+    toggles.attr('cx', left)
+    .attr('cy', 10)
+    .attr('r', 8)
+    .attr('fill', 'white')
+    .on('click', function() { handleMouseClick(this, left, right, switches) })
+    
+    let rects = groups.selectAll('rect.map')
+    rects.attr('width', maxWidth*(1-margin.left-margin.right)/24)
+    .attr('height', 20)
     .attr('x', (d, i) => horizontalScale(i))
     .attr('fill', (d) => colorScale(d))
+}
+
+function handleMouseOver(){
+    d3.select(this)
+    .attr('height', 30)
+    .attr('transform', 'translate(0, -5)')
+    .attr('stroke', 'black')
+}
+
+function handleMouseOut(){
+    d3.select(this)
+    .attr('height', 20)
+    .attr('transform', 'translate(0, 0)')
+    .attr('stroke', 'none')
+}
+
+function handleMouseClick(g, left, right, switches){
+    d3.select(g)
+    .attr('cx', (d) => {
+        d.on = d.on || false
+        let result =  d.on? left : right
+        d3.select(g.parentNode).select('.switch')
+        .attr('fill', d.on? 'silver' : 'black')
+        d.on = !d.on
+        return result
+    })
 }
