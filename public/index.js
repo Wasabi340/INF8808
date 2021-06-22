3e3ca7598cc7cabbb90511d5b87f59ca07d7c957
Alex <alex.trepanier@hotmail.com>
2021-06-21 22:36:20 -0400

    added metric placeholder

--- public/index.js
+++ public/index.js
@@ -2,12 +2,15 @@ import * as menu from './scripts/menu.js'
 import * as heatMaps from './scripts/heatMaps.js'
 import * as lineGraphs from './scripts/lineGraphs.js'
 import * as equation from './scripts/equation.js'
+import * as metric from './scripts/metric.js'
+
 
 function build(){
     menu.build()
     heatMaps.build()
     lineGraphs.build()
     equation.build()
+    metric.build()
 }
 
 build()
---
+++ public/scripts/metric.js
@@ -0,0 +1,21 @@
+export function build () {
+    console.log('building metric')
+    
+    d3.select('.metric svg')
+    .attr('width', '100%')
+    .attr('height', '100%')
+    
+    let g = d3.select('.metric svg')
+
+    let metric = g.select('text').node() ? g.select('text') : g.append('text')
+
+    let metricValue = 88
+
+    let maxWidth = g.node().getBoundingClientRect().width
+    let maxHeight = g.node().getBoundingClientRect().height
+
+    metric.text(`Aggregated metric : ${metricValue}`)
+    .attr('x', maxWidth/2)
+    .attr('y', maxHeight/2)
+    .attr('text-anchor', 'middle')
+}
