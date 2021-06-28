c97aa26dc5d324b45abe2626b8f1e381f4d694ae
Michael Tessier <michaeltessier92@Gmail.com>
2021-06-28 16:55:28 -0400

    index.js fetch csv-s

--- public/index.js
+++ public/index.js
@@ -7,21 +7,68 @@ import * as metric from './scripts/metric.js'
 import * as algorithm from './scripts/algorithm.js'
 
 
-function build(data){
-    menu.build(data)
-    heatMapsCases.build(data)
-    heatMapsDimensions.build(data)
-    lineGraphs.build(data)
-    equation.build(data)
-    metric.build(data)
-    algorithm.build(data)   
-}
+(function (d3) {
 
-d3.csv("../assets/data/test.csv").then(function(data) {
-    console.log(data[0]);
-    build(data)
+    Promise.all([
+        d3.csv("../assets/data/case1-25.csv", d3.autoType),
+        d3.csv("../assets/data/case1-50.csv", d3.autoType),
+        d3.csv("../assets/data/case2-25.csv", d3.autoType),
+        d3.csv("../assets/data/case2-50.csv", d3.autoType),
+        d3.csv("../assets/data/case3-25.csv", d3.autoType),
+        d3.csv("../assets/data/case3-50.csv", d3.autoType),
+        d3.csv("../assets/data/case4-25.csv", d3.autoType),
+        d3.csv("../assets/data/case4-50.csv", d3.autoType),
+        d3.csv("../assets/data/case5-25.csv", d3.autoType),
+        d3.csv("../assets/data/case5-50.csv", d3.autoType),
+        d3.csv("../assets/data/case6-25.csv", d3.autoType),
+        d3.csv("../assets/data/case6-50.csv", d3.autoType),
+        d3.csv("../assets/data/case7-25.csv", d3.autoType),
+        d3.csv("../assets/data/case7-50.csv", d3.autoType),
+        d3.csv("../assets/data/case8-25.csv", d3.autoType),
+        d3.csv("../assets/data/case8-50.csv", d3.autoType),
+        d3.csv("../assets/data/case9-25.csv", d3.autoType),
+        d3.csv("../assets/data/case9-50.csv", d3.autoType),
+        d3.csv("../assets/data/case10-25.csv", d3.autoType),
+        d3.csv("../assets/data/case10-50.csv", d3.autoType),
+        d3.csv("../assets/data/case11-25.csv", d3.autoType),
+        d3.csv("../assets/data/case11-50.csv", d3.autoType),
+        d3.csv("../assets/data/case12-25.csv", d3.autoType),
+        d3.csv("../assets/data/case12-50.csv", d3.autoType),
+        d3.csv("../assets/data/case13-25.csv", d3.autoType),
+        d3.csv("../assets/data/case13-50.csv", d3.autoType),
+        d3.csv("../assets/data/case14-25.csv", d3.autoType),
+        d3.csv("../assets/data/case14-50.csv", d3.autoType),
+        d3.csv("../assets/data/case15-25.csv", d3.autoType),
+        d3.csv("../assets/data/case15-50.csv", d3.autoType),
+        d3.csv("../assets/data/case16-25.csv", d3.autoType),
+        d3.csv("../assets/data/case16-50.csv", d3.autoType),
+        d3.csv("../assets/data/case17-25.csv", d3.autoType),
+        d3.csv("../assets/data/case17-50.csv", d3.autoType),
+        d3.csv("../assets/data/case18-25.csv", d3.autoType),
+        d3.csv("../assets/data/case18-50.csv", d3.autoType),
+        d3.csv("../assets/data/case19-25.csv", d3.autoType),
+        d3.csv("../assets/data/case19-50.csv", d3.autoType),
+        d3.csv("../assets/data/case20-25.csv", d3.autoType),
+        d3.csv("../assets/data/case20-50.csv", d3.autoType),
+    ]).then(function(cases) {
 
-    window.addEventListener('resize', () => {
-        build(data)
+        console.log(cases)
+        build()
+
+        function build(){
+            menu.build()
+            heatMaps.build()
+            lineGraphs.build()
+            equation.build()
+            metric.build()
+            algorithm.build()
+        }
+
+        window.addEventListener('resize', () => {
+            build()
+        })
+
+    }).catch(function(err) {
+        console.log(err)
     })
-});
+})(d3)
