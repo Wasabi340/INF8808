353dcceaba9f97a3ac4c607c690aa789c8032ea6
Louis-PhilippeGentile <louisphilippe.gentile@videotron.ca>
2021-06-28 21:41:33 -0400

    Removed dead code

--- public/scripts/algorithm.js
+++ public/scripts/algorithm.js
@@ -1,20 +1,3 @@
-function getFakeData(){
-    
-    let data = {
-        algorithms: []
-    }
-    
-    for(let i = 0; i < 5; i++){
-        
-        data.algorithms.push({
-            name:`Algorithm ${i + 1}`,
-            metric:Math.floor(Math.random() * 101),
-        })
-    }
-    
-    return data
-}
-
 function rearangeData(cases) {
 
     let data = {
